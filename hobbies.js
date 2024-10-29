8ba63ee18c3bd44a18212c26e98f20d8344e4819
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-29 15:08:47 +0100

    add: hobbies

---
+++ hobbies.js
@@ -0,0 +1,8 @@
+function hobbies(hobbyOne, hobbyTwo, hobbyThree){
+    this.hobbyOne = hobbyOne;
+    this.hobbyTwo = hobbyTwo;
+    this.hobbyThree = hobbyThree;
+}
+
+const sport = new hobbies ("basket", "calcio", "tennis");
+console.log(sport);
