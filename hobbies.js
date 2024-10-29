fb04cff9c6d3500eafbd88d3872934b37d443d0d
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-29 15:40:31 +0100

    fix: hobbies

--- hobbies.js
+++ hobbies.js
@@ -1,8 +1,10 @@
-function hobbies(hobbyOne, hobbyTwo, hobbyThree){
+let hobbies = [];
+function yourHobbies(hobbyOne, hobbyTwo, hobbyThree){
     this.hobbyOne = hobbyOne;
     this.hobbyTwo = hobbyTwo;
     this.hobbyThree = hobbyThree;
 }
 
-const sport = new hobbies ("basket", "calcio", "tennis");
-console.log(sport);
+const giulio = new yourHobbies ("basket", "lettura", "cinema");
+hobbies.push(giulio.hobbyOne, giulio.hobbyTwo, giulio.hobbyThree);
+console.log(hobbies);
